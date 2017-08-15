832e4f975448320f37b17c4f39f9de27dfc39d90
Petr Maronek <maronek.petr@tutamail.com>
2017-08-15 14:17:29 +0200

    parsing script finished

--- parseHyperLink.ps1
+++ parseHyperLink.ps1
@@ -1,3 +1,23 @@
 #Author: Petr Maronek
 #Extract link from 'a href' HTML tag
 
+Clear-Host
+
+Try {
+    $urls = Get-Content C:\Users\pmaronek\Documents\projects_Local\repos\powershell\urlsRaw.txt
+    Write-Host "File loaded" -ForegroundColor Yellow
+
+    $result = @()
+
+    foreach($url in $urls){
+        $array = $url.Split(">").Split("<")
+        $result += $array[2]
+    }
+
+    Write-Host $result 
+
+    Write-Host "URLs extracted successfully"  -ForegroundColor Green
+}
+Catch {
+    Write-Host "Error loading file" -ForegroundColor Red
+}
