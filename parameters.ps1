77ad36773b554e45c69fb4204a31880d27f27f62
Petr Maronek <maronek.petr@tutamail.com>
2017-08-15 16:43:07 +0200

    parameters and prompt added

--- parameters.ps1
+++ parameters.ps1
@@ -1,3 +1,18 @@
 #Author: Petr Maronek
 #Sandbox: Launching scripts with parameters
 
+param([Int32]$userParam)
+
+Clear-Host
+
+Write-Host "Parameter of the script is:" $userParam
+
+$userInput = Read-Host -Prompt "Input Today's Batch number"
+
+if([string]::IsNullOrEmpty($userInput)) {
+    Write-Host "You didn't supplied any Batch number!" -ForegroundColor Red
+}
+else{
+    [int]$BatchNum = [convert]::ToInt32($userInput,10)
+    Write-Host "Batch number is:" $userInput
+}
