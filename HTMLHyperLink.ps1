e49dd5c37da46cea6b8eedf082fbc637b29583c9
MahonyCZ13 <maronek.petr@gmail.com>
2017-08-17 12:16:11 +0200

    HTML Hyperlink parser completed

---
+++ HTMLHyperLink.ps1
@@ -0,0 +1,31 @@
+# Author Petr MAronek
+# Parse HTML formatted hyperlinks
+
+Clear-Host
+
+Try {
+    $urls = Get-Content .\SourceFiles\rawData.txt
+    Write-Host "File loaded successfuly. Parsing links..." -ForegroundColor Yellow
+
+    Try {
+
+        $result = @()
+
+        foreach($url in $urls) {
+            $array = $url.Split(">").Split("<")
+            $result += $array[2]
+        }
+
+        $result | Out-File -FilePath .\OutputFiles\parsedUrls.txt -Encoding ascii
+
+        Write-Host "Urls parsed successfuly. Opening file..." -ForegroundColor Green
+
+        Invoke-Item .\OutputFiles\parsedUrls.txt
+    }
+    Catch {
+        Write-Host "Error in parsing URLs" -ForegroundColor Red
+    }
+}
+Catch {
+    Write-Host "Error loading file." -ForegroundColor Red
+}
