c74c05c0518ff20e2d6194719507f675c2ab56b5
MahonyCZ13 <maronek.petr@gmail.com>
2017-08-17 12:01:30 +0200

    sharegate csv generator completed

---
+++ sharegateCSVGen.ps1
@@ -0,0 +1,33 @@
+#Author: Petr Maronek
+#Generate CSV for migration script
+
+Clear-Host
+
+$curentPath = Get-Location
+$sourceLocation = "\SourceFiles\"
+$sourceFileName = "urlsRaw.txt"
+
+Try {
+    
+    $urls = Get-Content $curentPath$sourceLocation$sourceFileName
+    Write-Host "File loaded" -ForegroundColor Yellow
+
+    "SourceUrl,TargetUrl,owner,batch" | Out-File -FilePath ".\OutputFiles\exported_URLs.csv" -Encoding ascii
+
+    foreach($url in $urls){
+        $array = $url.Split(">").Split("<").Split("#").Split(";").Split("")
+        $resultString = $array[3] + "," + $array[9] + "," + $array[14] + "," + $array[-1]
+        $resultString | Out-File -FilePath ".\OutputFiles\exported_URLs.csv" -Append -Encoding ascii
+    }
+
+    Write-Host "CSV generated successfuly. Opening file..."  -ForegroundColor Green
+
+    Invoke-Item ".\OutputFiles\exported_URLs.csv"
+
+}
+
+Catch {
+
+    Write-Host "Error loading file" -ForegroundColor Red
+
+}
