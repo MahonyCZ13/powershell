c78d9f65d9e5ea59fc94cd46a82e935f9fac0c76
Petr Maronek <MahonyCZ13@users.noreply.github.com>
2018-03-16 17:14:35 +0100

    script rewritten as module

--- dev/setTSProject.ps1
+++ dev/setTSProject.ps1
@@ -9,203 +9,224 @@
 .NOTES
     Author: Petr Maronek
 .LINK
-    https://insidecurlybrackets.azurewebsites.net/
+    https://www.msdit.cz/
 #>
 
-Clear-Host
+Function Create-Project() {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True, ParameterSetName="TypeScript", HelpMessage="Type a framework, you want to create a project for.")]
+        [String]$ProjectType
+    )
 
-Write-Host @"
+    switch($ProjectType) {
+        "TypeScript" {Create-Typescript}
+        "React" {Create-React}
+    }
+}
 
- #######################
- #######################
- #######################
- ###        ####/     )#
- ######  #######   #####
- ######  #######/    /##
- ######  ###########  ##
- ######  ######(     /##
- #######################
+Function Create-TypeScript {
+    Clear-Host
+
+    Write-Host @"
+
+    #######################
+    #######################
+    #######################
+    ###        ####/     )#
+    ######  #######   #####
+    ######  #######/    /##
+    ######  ###########  ##
+    ######  ######(     /##
+    #######################
 "@ -ForegroundColor Blue
 
-Write-Host "`n Welcome to Typescript project creation script!" -ForegroundColor Yellow
-Write-Host " Let us get started`n" -ForegroundColor Yellow
+    Write-Host "`n Welcome to Typescript project creation script!" -ForegroundColor Yellow
+    Write-Host " Let us get started`n" -ForegroundColor Yellow
 
-[string]$nodeInstallation = Read-Host -Prompt "First thigs first. Do you have NodeJS with NPM installed? (Y/n)"
+    [string]$nodeInstallation = Read-Host -Prompt "First thigs first. Do you have NodeJS with NPM installed? (Y/n)"
 
-if($nodeInstallation -Match "y" -or $nodeInstallation -Match $null) {
-    Write-Host "`nGreat!" -ForegroundColor Yellow
-}
-elseif($nodeInstallation -Match "n") {
-    Write-Host "`nSorry, you need to install NodeJS. You can get from here: https://nodejs.org/en/`n"
-    Break
-}
-else {
-    Write-Host "`nYour Answer was not recognized. Please launch the script again..." -ForegroundColor Red
-    Break
-}
-
-######## Project name default value handler ########
-$projectDefaultValue = "hello-world"
-$projectName = Read-Host -Prompt "`nWhat is the name of the project? ($projectDefaultValue)"
-$projectName = ($projectDefaultValue, $projectName)[[bool]$projectName]
-Write-Host "`nGot it! Give me a sec to do some basic stuff for you..." -ForegroundColor Yellow
-
-######## Set Variables ########
-Write-Host "`nSetting variables..."
-$configFolder = 'config'
-$stepBack = '../'
-$tsContent = @"
-export {};
-
-class Program {
-    public greeting: string;
-
-    constructor(text: string) {
-        this.greeting = text;
+    if($nodeInstallation -Match "y" -or $nodeInstallation -Match $null) {
+        Write-Host "`nGreat!" -ForegroundColor Yellow
     }
-    public greet() {
-        console.log(``Hello, $`{this.greeting}``);
+    elseif($nodeInstallation -Match "n") {
+        Write-Host "`nSorry, you need to install NodeJS. You can get from here: https://nodejs.org/en/`n"
+        Break
     }
-}
-let ge = new Program("User!");
-ge.greet();
+    else {
+        Write-Host "`nYour Answer was not recognized. Please launch the script again..." -ForegroundColor Red
+        Break
+    }
+
+    ######## Project name default value handler ########
+    $projectDefaultValue = "hello-world"
+    $projectName = Read-Host -Prompt "`nWhat is the name of the project? ($projectDefaultValue)"
+    $projectName = ($projectDefaultValue, $projectName)[[bool]$projectName]
+    Write-Host "`nGot it! Give me a sec to do some basic stuff for you..." -ForegroundColor Yellow
+
+    ######## Set Variables ########
+    Write-Host "`nSetting variables..."
+    $configFolder = 'config'
+    $stepBack = '../'
+    $tsContent = @"
+    export {};
+
+    class Program {
+        public greeting: string;
+
+        constructor(text: string) {
+            this.greeting = text;
+        }
+        public greet() {
+            console.log(``Hello, $`{this.greeting}``);
+        }
+    }
+    let ge = new Program("User!");
+    ge.greet();
 
 "@
-$tsConfigContents = @"
-{
-    "compilerOptions": {
-        "rootDir": "../src/ts",
-        "outDir": "../src/js",
-        "target": "es5",
-        "typeRoots": [
-            "./typings"
+    $tsConfigContents = @"
+    {
+        "compilerOptions": {
+            "rootDir": "../src/ts",
+            "outDir": "../src/js",
+            "target": "es5",
+            "typeRoots": [
+                "./typings"
+            ],
+            "sourceMap": true
+        },
+        "include": [
+            "../src/ts/*",
+            "./typings/*"
         ],
-        "sourceMap": true
-    },
-    "include": [
-        "../src/ts/*",
-        "./typings/*"
-    ],
-    "exclude": [
-        "node_modules"
-    ],
-    "files": [
-        "../src/ts/first.ts",
-        "../src/ts/second.ts",
-        "../src/ts/third.ts"
-    ]
-}
+        "exclude": [
+            "node_modules"
+        ],
+        "files": [
+            "../src/ts/first.ts",
+            "../src/ts/second.ts",
+            "../src/ts/third.ts"
+        ]
+    }
 "@
-Write-Host "`nDone!" -ForegroundColor Green
+    Write-Host "`nDone!" -ForegroundColor Green
 
-Write-Host "`n----------------------------" -ForegroundColor Blue
-Write-Host " Creating structure..."
-Write-Host "----------------------------" -ForegroundColor Blue
+    Write-Host "`n----------------------------" -ForegroundColor Blue
+    Write-Host " Creating structure..."
+    Write-Host "----------------------------" -ForegroundColor Blue
 
-######## Check, if project already exists ########
-Try 
-{
-    New-Item -ItemType Directory $projectName -ErrorAction Stop | Out-Null
-}
-Catch
-{
-    $errorMessage = $_
-    Write-host "Error Occured!!" -ForegroundColor Red
-    Write-Host $errorMessage`n -ForegroundColor Red
-    Write-Host "Exiting...`n" -ForegroundColor Yellow
+    ######## Check, if project already exists ########
+    Try 
+    {
+        New-Item -ItemType Directory $projectName -ErrorAction Stop | Out-Null
+    }
+    Catch
+    {
+        $errorMessage = $_
+        Write-host "Error Occured!!" -ForegroundColor Red
+        Write-Host $errorMessage`n -ForegroundColor Red
+        Write-Host "Exiting...`n" -ForegroundColor Yellow
+        Break
+    }
+
+    Set-Location -Path $projectName
+    New-Item -ItemType Directory 'src' | Out-Null
+    Set-Location -Path 'src' | Out-Null
+    New-Item -ItemType Directory 'js' | Out-Null
+    New-Item -ItemType Directory 'ts' | Out-Null
+    New-Item -ItemType File 'index.html' | Out-Null
+
+    ######## Create example TS file ########
+    Set-Location -Path 'ts' | Out-Null
+    New-Item -ItemType File 'hello.ts' | Out-Null
+    Try 
+    {
+        $tsContent | Set-Content 'hello.ts' -ErrorAction Stop
+    }
+    Catch {
+        $errorMessage = $_
+        Write-host "Error Occured!!" -ForegroundColor Red
+        Write-Host $errorMessage`n -ForegroundColor Red
+        Write-Host "Exiting...`n" -ForegroundColor Yellow
+        Break
+    }
+    1..2 | ForEach-Object {Set-Location $stepBack}
+
+    ######## Configure 'config' directory ########
+    New-Item -ItemType Directory $configFolder | Out-Null
+    Set-Location -Path $configFolder | Out-Null
+    New-Item -ItemType Directory 'typings' | Out-Null
+    New-Item -ItemType File 'tsconfig.json' | Out-Null
+    Try 
+    {
+        $tsConfigContents | Set-Content 'tsconfig.json' -ErrorAction Stop
+    }
+    Catch {
+        $errorMessage = $_
+        Write-host "Error Occured!!" -ForegroundColor Red
+        Write-Host $errorMessage`n -ForegroundColor Red
+        Write-Host "Exiting...`n" -ForegroundColor Yellow
+        Break
+    }
+    Set-Location -Path $stepBack
+    Write-Host "`nDone!" -ForegroundColor Green
     Break
-}
+    ######## Confiure NPM Environment ########
+    Write-Host "`nNow we will setup our NPM envirnment" -ForegroundColor Yellow
 
-Set-Location -Path $projectName
-New-Item -ItemType Directory 'src' | Out-Null
-Set-Location -Path 'src' | Out-Null
-New-Item -ItemType Directory 'js' | Out-Null
-New-Item -ItemType Directory 'ts' | Out-Null
-New-Item -ItemType File 'index.html' | Out-Null
+    Write-Host "`n----------------------------" -ForegroundColor Blue
+    Write-Host " NPM initialization "
+    Write-Host "----------------------------`n" -ForegroundColor Blue
 
-######## Create example TS file ########
-Set-Location -Path 'ts' | Out-Null
-New-Item -ItemType File 'hello.ts' | Out-Null
-Try {
-    $tsContent | Set-Content 'hello.ts' -ErrorAction Stop
-}
-Catch {
-    $errorMessage = $_
-    Write-host "Error Occured!!" -ForegroundColor Red
-    Write-Host $errorMessage`n -ForegroundColor Red
-    Write-Host "Exiting...`n" -ForegroundColor Yellow
-    Break
-}
-1..2 | ForEach-Object {Set-Location $stepBack}
+    npm init
 
-######## Configure 'config' directory ########
-New-Item -ItemType Directory $configFolder | Out-Null
-Set-Location -Path $configFolder | Out-Null
-New-Item -ItemType Directory 'typings' | Out-Null
-New-Item -ItemType File 'tsconfig.json' | Out-Null
-Try {
-    $tsConfigContents | Set-Content 'tsconfig.json' -ErrorAction Stop
-}
-Catch {
-    $errorMessage = $_
-    Write-host "Error Occured!!" -ForegroundColor Red
-    Write-Host $errorMessage`n -ForegroundColor Red
-    Write-Host "Exiting...`n" -ForegroundColor Yellow
-    Break
-}
-Set-Location -Path $stepBack
-Write-Host "`nDone!" -ForegroundColor Green
-Break
-######## Confiure NPM Environment ########
-Write-Host "`nNow we will setup our NPM envirnment" -ForegroundColor Yellow
+    Write-Host "`nDone!" -ForegroundColor Green
 
-Write-Host "`n----------------------------" -ForegroundColor Blue
-Write-Host " NPM initialization "
-Write-Host "----------------------------`n" -ForegroundColor Blue
+    ######## Install TypeScript ########
+    Write-Host "`nLet me install latest Typescript and all its dependencies for you..." -ForegroundColor Yellow
 
-npm init
+    Write-Host "`n----------------------------" -ForegroundColor Blue
+    Write-Host " Typescript setup "
+    Write-Host "----------------------------`n" -ForegroundColor Blue
 
-Write-Host "`nDone!" -ForegroundColor Green
+    npm install typescript@latest --save-dev
 
-######## Install TypeScript ########
-Write-Host "`nLet me install latest Typescript and all its dependencies for you..." -ForegroundColor Yellow
+    Write-Host "`nDone!" -ForegroundColor Green
 
-Write-Host "`n----------------------------" -ForegroundColor Blue
-Write-Host " Typescript setup "
-Write-Host "----------------------------`n" -ForegroundColor Blue
+    ######## Install ESLint ########
+    Write-Host "`nIt is time for us set up ESLint for some linting magic!" -ForegroundColor Yellow
 
-npm install typescript@latest --save-dev
+    Write-Host "`n----------------------------" -ForegroundColor Blue
+    Write-Host " ESLint setup "
+    Write-Host "----------------------------`n" -ForegroundColor Blue
+    Write-Host "Installing ESLint..." -ForegroundColor Yellow
 
-Write-Host "`nDone!" -ForegroundColor Green
+    npm install eslint --save-dev
 
-######## Install ESLint ########
-Write-Host "`nIt is time for us set up ESLint for some linting magic!" -ForegroundColor Yellow
+    Write-Host "`nDone!" -ForegroundColor Green
 
-Write-Host "`n----------------------------" -ForegroundColor Blue
-Write-Host " ESLint setup "
-Write-Host "----------------------------`n" -ForegroundColor Blue
-Write-Host "Installing ESLint..." -ForegroundColor Yellow
+    ######## Install TypeScript for ESLint ########
+    Write-Host "`nInstalling ESLint for Typescript..." -ForegroundColor Yellow
 
-npm install eslint --save-dev
+    npm install typescript-eslint-parser --save-dev
 
-Write-Host "`nDone!" -ForegroundColor Green
+    Write-Host "`nDone!" -ForegroundColor Green
 
-######## Install TypeScript for ESLint ########
-Write-Host "`nInstalling ESLint for Typescript..." -ForegroundColor Yellow
+    ######## Initialize ESLint for current project ########
+    Write-Host "`nConfiguring ESLint...`n" -ForegroundColor Yellow
 
-npm install typescript-eslint-parser --save-dev
+    eslint --init
 
-Write-Host "`nDone!" -ForegroundColor Green
+    Write-Host "`nDone!" -ForegroundColor Green
+    Write-Host @"
 
-######## Initialize ESLint for current project ########
-Write-Host "`nConfiguring ESLint...`n" -ForegroundColor Yellow
-
-eslint --init
-
-Write-Host "`nDone!" -ForegroundColor Green
-Write-Host @"
-
-Installation finished. You can now develop the next big thing!
-Happy coding!
+    Installation finished. You can now develop the next big thing!
+    Happy coding!
 
 "@ -ForegroundColor Yellow
+}
+
+Function Create-React {
+    Write-Host "React project creation under development..." -ForegroundColor Yellow
+}
