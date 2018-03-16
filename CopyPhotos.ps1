9e7e188d3762eaa2be40b0564fae51a9fcffe67d
Petr Maronek <MahonyCZ13@users.noreply.github.com>
2018-03-16 15:55:36 +0100

    Updated script header

--- CopyPhotos.ps1
+++ CopyPhotos.ps1
@@ -1,3 +1,17 @@
+# CopyPhotos.ps1
+<#
+.SYNOPSIS
+    Copy photos from folder structure
+.NAME
+    Photo Copy
+.DESCRIPTION
+    Copy photos from Google to OneDrive or from/to any other location
+.NOTES
+    Author: Petr Maronek
+.LINK
+    https://insidecurlybrackets.azurewebsites.net/
+#>
+
 Write-Host "Copying pictures to OneDrive..." -ForegroundColor Yellow
 Get-ChildItem "C:\Users\profile\Documents\Google Photos" -filter *.jpg -Recurse| ForEach-Object {Copy-Item -Path $_.FullName -Destination "C:\Users\profile\OneDrive\Pictures\Camera Roll" }
 Write-Host "Pictures successfully copied to OneDrive!" - -ForegroundColor Green
